a459704e132b2b215020e00eb45f07840ef548a6
thisvolwebnik(Nikita) <thisvolwebnik@yandex.ru>
2026-01-12 07:26:27 +0300

    refactor: split monolith layout into components

---
+++ src/components/AddTaskForm.jsx
@@ -0,0 +1,11 @@
+import { Field } from "./Field";
+import { Button } from "./Button";
+
+export const AddTaskForm = () => {
+  return (
+    <form className="todo__form">
+      <Field />
+      <Button />
+    </form>
+  );
+};
---
+++ src/components/Todo.jsx
@@ -0,0 +1,16 @@
+import { AddTaskForm } from "./AddTaskForm";
+import { SearchTaskForm } from "./SearchTaskForm";
+import { TodoInfo } from "./TodoInfo";
+import { TodoList } from "./TodoList";
+
+export const Todo = () => {
+  return (
+    <div className="todo">
+      <h1 className="todo__title">To Do List</h1>
+      <AddTaskForm />
+      <SearchTaskForm />
+      <TodoInfo />
+      <TodoList />
+    </div>
+  );
+};
